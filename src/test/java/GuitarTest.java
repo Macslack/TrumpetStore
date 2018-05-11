5bb64acb34d0321f40d1c7156703d16f6b5ca345
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-05-11 11:59:11 +0100

    adds passing guitar markup test

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -2,12 +2,18 @@ import instuments.Guitar;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
     Guitar guitar;
 
     @Before
     public void before(){
         guitar = new Guitar(100.00, 200.00, 5);
+    }
+    @Test
+    public void canGetMarkUp(){
+        assertEquals(100.00, guitar.calculateMarkup(),0.01);
     }
 
 
